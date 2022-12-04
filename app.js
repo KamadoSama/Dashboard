99991a9c879c1ecbd0c38f87e14a42bb952c8fd5
KamadoSama <tahoarthur@gmail.com>
2022-12-04 00:20:46 +0000

    main

---
+++ app.js
@@ -0,0 +1,9 @@
+let balise_i = document.getElementsByTagName('i')[1]
+var i = 0
+balise_i.addEventListener('click',function(e){
+    this.classList.add('fa-spin-pulse')
+    i +=1
+    if(i%2 ==0){
+        this.classList.remove('fa-spin-pulse')
+    }
+})
